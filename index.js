aef253d31f83a7e08778910e6be65fc8bf85a0b0
Devo-Dhruvil <devodhruvil@gmail.com>
2026-02-26 12:53:01 +0530

    Update TEST_ORDER_ID and trackingNumber values

--- index.js
+++ index.js
@@ -23,13 +23,13 @@ const EVENT_TAG_MAP = {
 const STATUS_TAGS = Object.values(EVENT_TAG_MAP);
 
 // 🔴 CHANGE THIS ORDER ID TO TEST
-const TEST_ORDER_ID = "12352343834955";
+const TEST_ORDER_ID = "12397845938507";
 
 // 🔴 FAKE PALLETFORCE RESPONSE
 const MOCK_TRACKING_DATA = [
   {
     eventCode: "POD",
-    trackingNumber: "1210225624571"
+    trackingNumber: "1210225625514"
   }
 ];
 
